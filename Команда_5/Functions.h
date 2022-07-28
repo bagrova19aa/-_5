5762c413ed1fbb58a703b1ea810fefe1cddf6c01
Анастасия Багрова <staska.nb18.na@gmail.com>
2022-07-28 12:47:08 +1000

    Вызов функции

--- Команда_5/Functions.h
+++ Команда_5/Functions.h
@@ -15,7 +15,7 @@ std::vector<Point> SortedVector(const std::vector<Point>& a, double max_dist, do
 void Matrix_Relationship_Distance(const std::vector<Point>& sorted_values, std::vector <std::vector<double>>& relationship, std::vector <std::vector<double>>& distance, double max_dist, double min_dist);
 
 //По ссылке изменяет(создаёт) матрицу энергий и словарь с вероятностями
-void EnergyMat_Probability (const std::vector <std::vector<double>>& base, std::vector <std::vector<double>>& energ, std::map <double, double>& val);
+void EnergyMat_Probability (const std::vector <std::vector<double>>& base, std::vector <std::vector<double>>& energ, std::map <double, double>& val, double max_dist, double min_dist);
 //Создает матрицу,ничего не возвращает,а изменяет уже существующие вектора
 void KirMat_Valence(const std::vector <std::vector<double>>& matr, std::vector <std::vector<double>>& kir, std::map <double, double>& val);
 
